b008c12c1a31aa95a7c07113978989f7c8eee9e5
RafdiZhafari <reyhanzhafari@gmail.com>
2021-12-23 22:29:41 +0700

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 //import './coba'
-import Car from './coba';
-
-const demo = new Car();
+//import Car from './coba';
+//import react from 'react';
+import './projekSib.css'
+import React, { useState,useEffect } from 'react';
 
 function App(){
-  <p id="app"></p>
-  document.getElementById("app").innerHTML = demo.cetak;
+  const [status,setStatus] = useState('positif');
+  function handleButton (event){
+    event.preventDefault();
+    if(status=='positif'){
+      setStatus('negatif')
+    }else{
+      setStatus('positif')
+    }
+  } 
+  return (
+    <div>
+        <div id="header-bg">
+            <h1 className="header">Projek Sib</h1>
+        </div>
+        <div id="input-form">
+            <h2 id="app-desc">Deteksi Sentimen Analisis</h2>
+            <div id="form-box0">
+                <form action=""  method="post" id="form-box" onSubmit={handleButton}>
+                    <textarea name="input" id="input-text" cols="30" rows="10"></textarea>
+                    <p id="output">{status}</p>
+                    <button type="submit" id="input-submit" >Cek</button>
+                </form>
+            </div>
+        </div>
+    </div>
+  )
 }
 //<p>Mobil saya mereknya {mobil}</p>
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
-import Car from './coba';
+import App from './App';
+//import Car from './coba';
 import reportWebVitals from './reportWebVitals';
 
-const messages = new Car();
-const pesan = messages.cetak
+//const messages = new Car();
+//const pesan = messages.cetak
 ReactDOM.render(
-  /*<React.StrictMode>
+  <React.StrictMode>
     <App />
-  </React.StrictMode>*/
-  <p id='coba'>{pesan}</p>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
